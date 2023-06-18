2e56056049f9fe838da6a4e41dca3ac11473f665
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2023-06-18 14:46:22 +0200

    add testing workbench for net

---
+++ test_wave2params.py
@@ -0,0 +1,71 @@
+# %%
+# imports
+import torch
+from fm_inversynth import Wave2Params, MelbandsEncoder, MFCCEncoder, PitchEncoder
+import ddsp_utils
+
+
+# %%
+# create synth & models
+
+synth = ddsp_utils.FMSynth(sr=48000)
+model = Wave2Params()
+melbands_encoder = MelbandsEncoder()
+mfcc_encoder = MFCCEncoder()
+pitch_encoder = PitchEncoder(num_frames=376)
+
+# %%
+# create input wave
+buffer_length = 48000 * 4
+carrier_frequency = torch.tensor([440.0]).repeat(buffer_length).unsqueeze(0)
+harmonicity_ratio = torch.tensor([4.0]).repeat(buffer_length).unsqueeze(0)
+modulation_index = torch.tensor([2.0]).repeat(buffer_length).unsqueeze(0)
+y = synth(carrier_frequency, harmonicity_ratio, modulation_index)
+y.shape
+
+
+# %%
+# function to save tensor to wav file
+
+def save_buffer_to_wav(buffer: torch.Tensor, filename: str, sr: int = 48000):
+    import soundfile as sf
+    assert len(buffer.shape) == 1
+    buffer_np = buffer.detach().cpu().numpy()
+    sf.write(filename, buffer_np, sr)
+
+
+# %%
+
+# save y to wav file
+save_buffer_to_wav(y, '/Users/balintl/Desktop/test_my_fmsynth.wav')
+
+
+# %%
+# create batches for testing
+
+y_batch = y.repeat(2, 1)
+y_batch.shape
+
+# %%
+# model forward pass
+
+y_pred, params_pred = model(y_batch)
+y_pred.shape, params_pred.shape
+
+
+# %%
+
+melbands = melbands_encoder(y_batch)
+melbands.shape
+
+# %%
+
+mfcc = mfcc_encoder(y_batch)
+mfcc.shape
+
+# %%
+
+pitch = pitch_encoder(y_batch)
+pitch.shape
+
+# %%
