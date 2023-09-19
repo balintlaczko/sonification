f115c740343192b0837ee865f4d8ef6c53c126a2
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2023-09-19 12:57:38 +0200

    decouple hidden from latent, add simple vae

--- simple_autoencoder.py
+++ simple_autoencoder.py
@@ -4,15 +4,16 @@ from torch.nn import functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, latent_size):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
+        self.latent_size = latent_size
         self.layers = nn.Sequential(
-            nn.Linear(input_size, input_size, dtype=torch.float32),
-            nn.ReLU(),
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
+            nn.Linear(input_size, hidden_size, dtype=torch.float32),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_size, latent_size),
+            nn.LeakyReLU(0.2),
         )
 
     def forward(self, x):
@@ -20,15 +21,16 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, latent_size, hidden_size, output_size):
         super().__init__()
+        self.latent_size = latent_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.layers = nn.Sequential(
+            nn.Linear(latent_size, hidden_size),
+            nn.LeakyReLU(0.2),
             nn.Linear(hidden_size, output_size),
-            nn.ReLU(),
-            nn.Linear(output_size, output_size),
-            nn.ReLU(),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
@@ -36,13 +38,38 @@ class Decoder(nn.Module):
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, latent_size, output_size):
         super().__init__()
-        self.encoder = Encoder(input_size, hidden_size)
-        self.decoder = Decoder(hidden_size, output_size)
+        self.encoder = Encoder(input_size, hidden_size, latent_size)
+        self.decoder = Decoder(latent_size, hidden_size, output_size)
 
     def forward(self, x):
         return self.decoder(self.encoder(x))
 
     def encode(self, x):
         return self.encoder(x)
+
+# inspired by https://medium.com/@rekalantar/variational-auto-encoder-vae-pytorch-tutorial-dce2d2fe0f5f
+
+
+class VAE(nn.Module):
+    def __init__(self, input_size, hidden_size, latent_size):
+        super().__init__()
+        self.encoder = Encoder(input_size, hidden_size, latent_size)
+        self.mu = nn.Linear(latent_size, latent_size)
+        self.logvar = nn.Linear(latent_size, latent_size)
+        self.decoder = Decoder(latent_size, hidden_size, input_size)
+
+    def encode(self, x):
+        x = self.encoder(x)
+        return self.mu(x), self.logvar(x)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x):
+        mean, logvar = self.encode(x)
+        z = self.reparameterize(mean, logvar)
+        return self.decoder(z), mean, logvar, z
