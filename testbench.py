1dec11d6f75bdd8a21d7406f5c6c5352480f223a
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2023-10-12 17:04:04 +0200

    fiddling with the masks and the 16-bit images

--- testbench.py
+++ testbench.py
@@ -280,3 +280,41 @@ target_name = "/Users/balintl/Desktop/Sonification Pilot Video/Materials/process
 # %%
 target_name = "/Users/balintl/Desktop/Sonification Pilot Video/Materials/processing/rfp_red.avi"
 videos2planes(video2red=video_rfp, target_name=target_name)
+
+
+# %%
+import cv2
+import numpy as np
+from utils import view
+
+# %%
+mask_path = "/Volumes/T7RITMO/Sonification/Amani_230117/W3onlywith masks/Timepoint_001_230117-BST_C03_s1_w3_cp_masks.png"
+image_w1_path = "/Volumes/T7RITMO/Sonification/Amani_230117/W1W2/Timepoint_001_230117-BST_C03_s1_w1.TIF"
+image_w2_path = "/Volumes/T7RITMO/Sonification/Amani_230117/W1W2/Timepoint_001_230117-BST_C03_s1_w2.TIF"
+mask = cv2.imread(mask_path, cv2.IMREAD_ANYDEPTH)
+w1 = cv2.imread(image_w1_path, cv2.IMREAD_ANYDEPTH)
+w2 = cv2.imread(image_w2_path, cv2.IMREAD_ANYDEPTH)
+np.unique(mask)
+
+# %%
+label_1 = np.where(mask == 1, 1, 0)
+view(label_1.astype(np.uint8) * 255, 0.3)
+
+# %%
+# view normalized rgb
+all_rgb = np.dstack((w1, w2, np.zeros_like(w1)))
+all_rgb_8bit = (all_rgb / np.max(all_rgb) * 255).astype(np.uint8)
+view(all_rgb_8bit, 0.3)
+
+# %%
+# mask original imige at a given label
+label = 42
+w1_masked = np.where(mask == label, w1, 0)
+w2_masked = np.where(mask == label, w2, 0)
+# make rgb image
+rgb = np.dstack((w1_masked, w2_masked, np.zeros_like(w1_masked)))
+rgb_8bit = (rgb / np.max(rgb) * 255).astype(np.uint8)
+view(rgb_8bit, 0.3)
+
+# %%
+
