5043245c8d5a2b91ca428f5b6843500254fbaf90
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2024-02-10 11:08:45 +0100

    add kld_loss

--- sonification/models/loss.py
+++ sonification/models/loss.py
@@ -3,6 +3,17 @@ from torch import nn
 from torch import Tensor
 
 
+def kld_loss(mu: Tensor, logvar: Tensor) -> Tensor:
+    """
+    Compute the Kullback-Leibler divergence loss.
+    :param mu: (Tensor) Mean of the latent Gaussian
+    :param logvar: (Tensor) Standard deviation of the latent Gaussian
+    :return: (Tensor) KLD loss
+    """
+    kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
+    return kld.mean()
+
+
 # taken form: https://github.com/AntixK/PyTorch-VAE/blob/master/models/info_vae.py
 # then modified it into its own class
 class MMDloss(nn.Module):
