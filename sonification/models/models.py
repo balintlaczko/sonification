659e199a7f05b6c2e92fc8b6a4d13897ddb9affb
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2024-02-12 18:05:51 +0100

    use factorvae recon_loss + bugfixes

--- sonification/models/models.py
+++ sonification/models/models.py
@@ -1,12 +1,15 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from .layers import *
 from lightning.pytorch import LightningModule
 from lightning.pytorch.loggers import TensorBoardLogger
 from ..utils.tensor import permute_dims
-from .loss import kld_loss
+from .loss import kld_loss, recon_loss
 from piqa import SSIM
+import matplotlib.pyplot as plt
+import os
 
 
 class AE(nn.Module):
@@ -86,7 +89,7 @@ class PlFactorVAE(LightningModule):
 
         # data params
         self.in_channels = args.in_channels
-        self.input_size = args.input_size
+        self.input_size = args.img_size
         self.hidden_size = args.hidden_size
         self.latent_size = args.latent_size
         self.layers_channels = args.layers_channels
@@ -94,7 +97,8 @@ class PlFactorVAE(LightningModule):
         self.d_num_layers = args.d_num_layers
 
         # losses
-        self.mse = nn.MSELoss()
+        # self.mse = nn.MSELoss()
+        self.bce = recon_loss
         # self.ssim = SSIM(n_channels=in_channels)
         self.kld = kld_loss
         self.kld_weight = args.kld_weight
@@ -106,6 +110,10 @@ class PlFactorVAE(LightningModule):
         self.lr_vae = args.lr_vae
         self.lr_decay_vae = args.lr_decay_vae
         self.lr_d = args.lr_d
+
+        # logging
+        self.plot_interval = args.plot_interval
+        self.args = args
 
         # models
         self.VAE = ConvVAE(self.in_channels, self.hidden_size,
@@ -123,6 +131,7 @@ class PlFactorVAE(LightningModule):
         return self.VAE.decoder(z)
 
     def training_step(self, batch, batch_idx):
+        self.VAE.train()
         # get the optimizers and schedulers
         # vae_optimizer, d_optimizer = self.optimizers()
         vae_optimizer = self.optimizers()
@@ -140,7 +149,8 @@ class PlFactorVAE(LightningModule):
         x_recon, mean, logvar, z = self.VAE(x_1)
 
         # VAE reconstruction loss
-        vae_recon_loss = self.mse(x_recon, x_1 * self.onpix_weight)
+        # vae_recon_loss = self.mse(x_recon, x_1 * self.onpix_weight)
+        vae_recon_loss = self.bce(x_recon, x_1)
         # vae_recon_loss = 1 - self.ssim(x_recon, x_1)
         # vae_recon_loss = self.mse(x_recon, x_1) + (1 - self.ssim(x_recon, x_1))
 
@@ -191,73 +201,121 @@ class PlFactorVAE(LightningModule):
             "vae_l1_penalty": l1_penalty
         }, on_step=False, on_epoch=True)
 
-    def validation_step(self, batch, batch_idx):
-        # get the batch
-        x_1, x_2 = batch
-        batch_size = x_1.shape[0]
-        # print('batch_size:', batch_size)
+    # def validation_step(self, batch, batch_idx):
+    #     # get the batch
+    #     x_1, x_2 = batch
+    #     batch_size = x_1.shape[0]
+    #     # print('batch_size:', batch_size)
 
-        # create a batch of ones and zeros for the discriminator
-        # ones = torch.ones(batch_size, dtype=torch.long, device=self.device)
-        # zeros = torch.zeros(batch_size, dtype=torch.long, device=self.device)
+    #     # create a batch of ones and zeros for the discriminator
+    #     # ones = torch.ones(batch_size, dtype=torch.long, device=self.device)
+    #     # zeros = torch.zeros(batch_size, dtype=torch.long, device=self.device)
 
-        # VAE forward pass
-        x_recon, mean, logvar, z = self.VAE(x_1)
+    #     # VAE forward pass
+    #     x_recon, mean, logvar, z = self.VAE(x_1)
 
-        # VAE reconstruction loss
-        vae_recon_loss = self.mse(x_recon, x_1 * self.onpix_weight)
-        # vae_recon_loss = 1 - self.ssim(x_recon, x_1)
-        # vae_recon_loss = self.mse(x_recon, x_1) + (1 - self.ssim(x_recon, x_1))
+    #     # VAE reconstruction loss
+    #     vae_recon_loss = self.mse(x_recon, x_1 * self.onpix_weight)
+    #     # vae_recon_loss = 1 - self.ssim(x_recon, x_1)
+    #     # vae_recon_loss = self.mse(x_recon, x_1) + (1 - self.ssim(x_recon, x_1))
 
-        # VAE KLD loss
-        kld_loss = self.kld(mean, logvar) * self.kld_weight
+    #     # VAE KLD loss
+    #     kld_loss = self.kld(mean, logvar) * self.kld_weight
 
-        # VAE TC loss
-        # d_z = self.D(z)
-        # vae_tc_loss = (d_z[:, 0] - d_z[:, 1]).mean() * self.tc_weight
+    #     # VAE TC loss
+    #     # d_z = self.D(z)
+    #     # vae_tc_loss = (d_z[:, 0] - d_z[:, 1]).mean() * self.tc_weight
 
-        # L1 penalty
-        l1_penalty = sum([p.abs().sum()
-                         for p in self.VAE.parameters()]) * self.l1_weight
+    #     # L1 penalty
+    #     l1_penalty = sum([p.abs().sum()
+    #                      for p in self.VAE.parameters()]) * self.l1_weight
 
-        # VAE loss
-        # vae_loss = vae_recon_loss + kld_loss + vae_tc_loss + l1_penalty
-        vae_loss = vae_recon_loss + kld_loss + l1_penalty
+    #     # VAE loss
+    #     # vae_loss = vae_recon_loss + kld_loss + vae_tc_loss + l1_penalty
+    #     vae_loss = vae_recon_loss + kld_loss + l1_penalty
 
-        # Discriminator forward pass
-        # mean_2, logvar_2 = self.VAE.encode(x_2)
-        # z_2 = self.VAE.reparameterize(mean_2, logvar_2)
-        # z_2_perm = permute_dims(z_2)
-        # d_z_2_perm = self.D(z_2_perm)
-        # d_tc_loss = 0.5 * (F.cross_entropy(d_z, zeros) + F.cross_entropy(d_z_2_perm, ones))
+    #     # Discriminator forward pass
+    #     # mean_2, logvar_2 = self.VAE.encode(x_2)
+    #     # z_2 = self.VAE.reparameterize(mean_2, logvar_2)
+    #     # z_2_perm = permute_dims(z_2)
+    #     # d_z_2_perm = self.D(z_2_perm)
+    #     # d_tc_loss = 0.5 * (F.cross_entropy(d_z, zeros) + F.cross_entropy(d_z_2_perm, ones))
 
-        # log the losses
-        self.log_dict({
-            "val_vae_loss": vae_loss,
-            "val_vae_recon_loss": vae_recon_loss,
-            "val_vae_kld_loss": kld_loss,
-            # "val_vae_tc_loss": vae_tc_loss,
-            # "val_d_tc_loss": d_tc_loss
-            "val_vae_l1_penalty": l1_penalty
-        }, on_step=False, on_epoch=True)
+    #     # log the losses
+    #     self.log_dict({
+    #         "val_vae_loss": vae_loss,
+    #         "val_vae_recon_loss": vae_recon_loss,
+    #         "val_vae_kld_loss": kld_loss,
+    #         # "val_vae_tc_loss": vae_tc_loss,
+    #         # "val_d_tc_loss": d_tc_loss
+    #         "val_vae_l1_penalty": l1_penalty
+    #     }, on_step=False, on_epoch=True)
 
-    def on_validation_end(self) -> None:
-        # get a batch of images
-        num_val_samples = len(self.val_dataset)
-        # get a random image
-        idx = torch.randint(0, num_val_samples, (1,)).item()
-        x_1, x_2 = self.val_dataset[idx]
-        x_1, x_2 = x_1.unsqueeze(0).cuda(), x_2.unsqueeze(0).cuda()
+    def on_train_epoch_end(self) -> None:
+        self.VAE.eval()
+        # get the epoch number from trainer
+        epoch = self.trainer.current_epoch
 
-        x_1_recon, mean, logvar, z = self.VAE(x_1)
-        x_2_recon, mean, logvar, z = self.VAE(x_2)
+        if epoch % self.plot_interval != 0:
+            return
 
-        # log the images
-        self.log_tb_images([[x_1.squeeze(0), x_1_recon.squeeze(0)],
-                           [x_2.squeeze(0), x_2_recon.squeeze(0)]])
+        self.save_recon_plot()
+        self.save_latent_space_plot()
+
+    def save_recon_plot(self, num_recons=4):
+        """Save a figure of N reconstructions"""
+        fig, ax = plt.subplots(2, num_recons, figsize=(20, 10))
+        # get the length of the training dataset
+        dataset = self.trainer.train_dataloader.dataset
+        for i in range(num_recons):
+            # get a random index
+            idx = torch.randint(0, len(dataset), (1,)).item()
+            x, _ = dataset[idx]
+            x_in = x.unsqueeze(0).to(self.device)
+            x_recon, mean, logvar, z = self.VAE(x_in)
+            x_recon = x_recon[0, 0, ...].detach().cpu().numpy()
+            # plot ground truth image
+            ax[0, i].imshow(x[0, ...], cmap="gray")
+            ax[0, i].set_title(f"GT_{idx}")
+            # plot reconstruction
+            ax[1, i].imshow(x_recon, cmap="gray")
+            ax[1, i].set_title(f"Recon_{idx}")
+        # save figure to checkpoint folder/recons
+        save_dir = os.path.join(self.args.ckpt_path,
+                                self.args.ckpt_name, "recons")
+        os.makedirs(save_dir, exist_ok=True)
+        fig_name = f"recons_{str(self.trainer.current_epoch).zfill(5)}.png"
+        plt.savefig(os.path.join(save_dir, fig_name))
+        plt.close()
+
+    def save_latent_space_plot(self, batch_size=128):
+        """Save a figure of the latent space"""
+        # get the length of the training dataset
+        dataset = self.trainer.train_dataloader.dataset
+        loader = DataLoader(dataset, batch_size=batch_size,
+                            shuffle=False, drop_last=False)
+        z_all = torch.zeros(
+            len(dataset), self.args.latent_size).to(self.device)
+        for batch_idx, data in enumerate(loader):
+            x, y = data
+            x_recon, mean, logvar, z = self.VAE(x.to(self.device))
+            z = z.detach()
+            z_all[batch_idx*batch_size: batch_idx*batch_size + batch_size] = z
+        z_all = z_all.cpu().numpy()
+        # create the figure
+        fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+        ax.scatter(z_all[:, 0], z_all[:, 1])
+        ax.set_title(
+            f"Latent space at epoch {self.trainer.current_epoch}")
+        # save figure to checkpoint folder/latent
+        save_dir = os.path.join(self.args.ckpt_path,
+                                self.args.ckpt_name, "latent")
+        os.makedirs(save_dir, exist_ok=True)
+        fig_name = f"latent_{str(self.trainer.current_epoch).zfill(5)}.png"
+        plt.savefig(os.path.join(save_dir, fig_name))
+        plt.close()
 
     def log_tb_images(self, viz_batch) -> None:
-
         # Get tensorboard logger
         tb_logger = None
         for logger in self.trainer.loggers:
@@ -279,8 +337,8 @@ class PlFactorVAE(LightningModule):
 
     def configure_optimizers(self):
         vae_optimizer = torch.optim.Adam(
-            self.VAE.parameters(), lr=self.hparams.lr_vae, betas=(0.9, 0.999))
+            self.VAE.parameters(), lr=self.lr_vae, betas=(0.9, 0.999))
         # d_optimizer = torch.optim.Adam(self.D.parameters(), lr=self.hparams.lr_d, betas=(0.5, 0.9))
         vae_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-            vae_optimizer, gamma=self.hparams.lr_decay_vae)
+            vae_optimizer, gamma=self.lr_decay_vae)
         return [vae_optimizer], [vae_scheduler]
