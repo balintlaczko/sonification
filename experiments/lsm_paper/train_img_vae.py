104c507fb510bc095d1630c3b428a0708c451c66
Balint Laczko <balintlaczko01@gmail.com>
2024-03-03 14:44:20 +0100

    removed unused params, better kld management

--- experiments/lsm_paper/train_img_vae.py
+++ experiments/lsm_paper/train_img_vae.py
@@ -6,7 +6,7 @@ import random
 import numpy as np
 from lightning.pytorch import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
-from lightning.pytorch.loggers import CSVLogger, TensorBoardLogger
+from lightning.pytorch.loggers import TensorBoardLogger
 from sonification.models.models import PlFactorVAE
 from sonification.datasets import White_Square_dataset
 
@@ -25,17 +25,17 @@ def main():
 
     # dataset
     parser.add_argument('--csv_path', type=str,
-                        default='white_squares_xy_16_4.csv', help='csv path')
-    parser.add_argument('--img_size', type=int, default=16, help='image size')
+                        default='white_squares_xy_64_2.csv', help='csv path')
+    parser.add_argument('--img_size', type=int, default=64, help='image size')
     parser.add_argument('--square_size', type=int,
-                        default=4, help='size of the square')
+                        default=2, help='size of the square')
 
     # model
     parser.add_argument('--in_channels', type=int,
                         default=1, help='image color channels')
     parser.add_argument('--latent_size', type=int,
                         default=2, help='latent size')
-    parser.add_argument('--layers_channels', type=int, nargs='*', default=[512, 256, 1024],
+    parser.add_argument('--layers_channels', type=int, nargs='*', default=[64, 128, 256, 512],
                         help='channels for the layers')
     parser.add_argument('--d_hidden_size', type=int,
                         default=512, help='mlp hidden size')
@@ -46,25 +46,27 @@ def main():
     parser.add_argument('--train_epochs', type=int,
                         default=1000000, help='number of training epochs')
     parser.add_argument('--batch_size', type=int,
-                        default=144, help='batch size')
+                        default=512, help='batch size')
     parser.add_argument('--dataset_size', type=int,
-                        default=144, help='batch size')
-    parser.add_argument('--lr_vae', type=float, default=0.03,
+                        default=3844, help='batch size')
+    parser.add_argument('--lr_vae', type=float, default=1e-2,
                         help='learning rate for the vae')
-    parser.add_argument('--lr_decay_vae', type=float, default=0.99)
-    parser.add_argument('--lr_d', type=float, default=1e-4,
+    parser.add_argument('--lr_decay_vae', type=float, default=0.9999)
+    parser.add_argument('--lr_d', type=float, default=1e-2,
                         help='learning rate for the discriminator')
-    parser.add_argument('--lr_decay_d', type=float, default=0.99)
-    parser.add_argument('--kld_weight', type=float,
-                        default=0.02, help='kld weight')
-    parser.add_argument('--mmd_weight', type=float,
-                        default=1e-1, help='mmd weight')
-    parser.add_argument('--mmd_prior_distribution', type=str,
-                        default='gaussian',)  # gaussian or uniform
+    parser.add_argument('--lr_decay_d', type=float, default=0.9999)
+    parser.add_argument('--recon_weight', type=float, default=20,
+                        help='reconstruction weight')
+    parser.add_argument('--kld_weight_max', type=float,
+                        default=0.02, help='kld weight at the end of the warmup')
+    parser.add_argument('--kld_weight_min', type=float, default=0.002,
+                        help='kld weight at the start of the warmup')
+    parser.add_argument('--kld_start_epoch', type=int, default=5000,
+                        help='the epoch at which to start the kld warmup from kld_weight_min to kld_weight_max')
+    parser.add_argument('--kld_warmup_epochs', type=int, default=10000,
+                        help='the number of epochs to warmup the kld weight')
     parser.add_argument('--tc_weight', type=float,
-                        default=1, help='tc weight')
-    parser.add_argument('--l1_weight', type=float,
-                        default=0, help='l1 weight')
+                        default=6, help='tc weight')
 
     # GPU
     parser.add_argument('--num_devices', type=int, nargs='*', default=[0],
@@ -74,20 +76,20 @@ def main():
     parser.add_argument('--ckpt_path', type=str,
                         default='./ckpt/white_squares_fvae_opt', help='checkpoint path')
     parser.add_argument('--ckpt_name', type=str,
-                        default='factorvae-opt-v4', help='checkpoint name')
+                        default='with_falloff-v12', help='checkpoint name')
     parser.add_argument('--resume_ckpt_path', type=str,
                         default=None,)
     parser.add_argument(
         '--logdir', type=str, default='./logs/white_squares_fvae_opt', help='log directory')
-    parser.add_argument('--plot_interval', type=int, default=100)
+    parser.add_argument('--plot_interval', type=int, default=10)
 
     # quick comment
-    parser.add_argument('--comment', type=str, default='less tc weight, faster lr decay',
+    parser.add_argument('--comment', type=str, default='no l1, but with gradient clipping',
                         help='add a comment if needed')
 
     args = parser.parse_args()
-    # manually calc & add weight for active pixels (assuming a sparse binary image)
-    args.onpix_weight = args.img_size / args.square_size
+    # args.onpix_weight = args.img_size // args.square_size
+    # args.onpix_weight = 1
 
     # create train dataset
     train_dataset = White_Square_dataset(
@@ -99,7 +101,7 @@ def main():
 
     # create train and val dataloaders
     train_loader = DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, pin_memory=True)
+        train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=4, prefetch_factor=2, persistent_workers=True,)
 
     # create the model
     model = PlFactorVAE(args)
@@ -109,7 +111,7 @@ def main():
     best_checkpoint_callback = ModelCheckpoint(
         monitor="vae_loss",
         dirpath=checkpoint_path,
-        filename=args.ckpt_name + "_val_{epoch:02d}-{val_loss:.4f}",
+        filename=args.ckpt_name + "_{epoch:02d}-{loss:.4f}",
         save_top_k=1,
         mode="min",
     )
@@ -122,16 +124,16 @@ def main():
     )
 
     # logger callbacks
-    csv_logger = CSVLogger(save_dir=args.logdir, name=args.ckpt_name)
     tensorboard_logger = TensorBoardLogger(
         save_dir=args.logdir, name=args.ckpt_name)
 
     trainer = Trainer(
         max_epochs=args.train_epochs,
         enable_checkpointing=True,
-        callbacks=[best_checkpoint_callback, last_checkpoint_callback],
-        logger=[csv_logger, tensorboard_logger],
-        log_every_n_steps=20,
+        callbacks=[best_checkpoint_callback,
+                   last_checkpoint_callback],
+        logger=tensorboard_logger,
+        log_every_n_steps=1,
     )
 
     hyperparameters = dict(
@@ -145,7 +147,11 @@ def main():
         lr_decay_vae=args.lr_decay_vae,
         lr_d=args.lr_d,
         lr_decay_d=args.lr_decay_d,
-        kld_weight=args.kld_weight,
+        recon_weight=args.recon_weight,
+        kld_weight_max=args.kld_weight_max,
+        kld_weight_min=args.kld_weight_min,
+        kld_start_epoch=args.kld_start_epoch,
+        kld_warmup_epochs=args.kld_warmup_epochs,
         tc_weight=args.tc_weight,
         comment=args.comment,
     )
@@ -156,4 +162,5 @@ def main():
 
 
 if __name__ == "__main__":
+    torch.set_float32_matmul_precision('high')
     main()
