868832122fe19465bb24b79da38882f23c54869d
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2025-03-20 10:21:11 +0100

    try abspath for logdir

--- experiments/fm_ddsp/train_fm_estimator.py
+++ experiments/fm_ddsp/train_fm_estimator.py
@@ -77,7 +77,10 @@ def main():
     callbacks = [best_checkpoint_callback, last_checkpoint_callback]
 
     # create logger
-    logdir = os.path.join(args.logdir, args.ckpt_name)
+    # change relative path to absolute path
+    logdir = os.path.abspath(args.logdir)
+    logdir = os.path.join(logdir, args.ckpt_name)
+    print(f"Logging to {logdir}")
     logger = WandbLogger(
         name=args.ckpt_name,
         project="fm_ddsp",
