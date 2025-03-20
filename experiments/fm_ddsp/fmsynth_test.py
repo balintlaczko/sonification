68abb28cd9eb92874da2133ca343c5babc39a4da
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2025-03-20 17:13:26 +0100

    add test

--- experiments/fm_ddsp/fmsynth_test.py
+++ experiments/fm_ddsp/fmsynth_test.py
@@ -1,5 +1,6 @@
 import torch
 from sonification.models.ddsp import FMSynth
+from sonification.utils.tensor import wrap
 
 # Create inputs
 carrier_frequency = torch.rand(1, 48000, requires_grad=True)
@@ -18,3 +19,10 @@ loss.backward()
 
 # If no errors and all gradients are computed, it's differentiable
 print(carrier_frequency.grad is not None)  # Should be True
+
+# Test differentiability
+phase = torch.tensor([0.5, 1.2, 2.7], requires_grad=True)
+wrapped = wrap(phase, 0.0, 1.0)
+loss = wrapped.sum()
+loss.backward()
+print("Gradient exists:", phase.grad is not None)  # Should print True
