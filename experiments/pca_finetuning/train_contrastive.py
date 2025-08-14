b7c49b1424b53f171bfb1fd22b2b4144a627f6cc
Balint Laczko <balint.laczko@imv.uio.no>
2025-08-14 20:07:45 +0200

    new version params

--- experiments/pca_finetuning/train_contrastive.py
+++ experiments/pca_finetuning/train_contrastive.py
@@ -23,14 +23,14 @@ def main():
     parser.add_argument("--n_mels", type=int, default=512)
     parser.add_argument("--power", type=float, default=1)
     parser.add_argument("--normalized", type=int, default=1)
-    parser.add_argument("--max_harm_ratio", type=int, default=8)
-    parser.add_argument("--max_mod_idx", type=int, default=8)
-    parser.add_argument("--num_views", type=int, default=4)  # number of views for contrastive learning
+    parser.add_argument("--max_harm_ratio", type=int, default=10)
+    parser.add_argument("--max_mod_idx", type=int, default=10)
+    parser.add_argument("--num_views", type=int, default=6)  # number of views for contrastive learning
     parser.add_argument("--apply_transposition", type=int, default=1)
-    parser.add_argument("--transposition_range", type=float, default=3.0)  # range for pitch transposition
-    parser.add_argument("--noise_max_amp", type=float, default=0.01)  # max amplitude for noise augmentation
+    parser.add_argument("--transposition_range", type=float, default=4.0)  # range for pitch transposition
+    parser.add_argument("--noise_max_amp", type=float, default=0.02)  # max amplitude for noise augmentation
     # model params
-    parser.add_argument("--latent_size", type=int, default=32)
+    parser.add_argument("--latent_size", type=int, default=8)
     parser.add_argument("--center_momentum", type=float, default=0.9)
     parser.add_argument("--ema_decay_min", type=float, default=0.996)
     parser.add_argument("--ema_decay_max", type=float, default=0.9999)
@@ -54,9 +54,9 @@ def main():
     parser.add_argument("--train_epochs", type=int, default=100000)
     parser.add_argument("--steps_per_epoch", type=int, default=100)
     parser.add_argument("--ckpt_path", type=str, default="./ckpt/fm_embedder")
-    parser.add_argument("--ckpt_name", type=str, default="imv_v4.1")
+    parser.add_argument("--ckpt_name", type=str, default="imv_v4.2")
     parser.add_argument("--logdir", type=str, default="./logs/fm_embedder")
-    parser.add_argument("--comment", type=str, default="higher dim, bigger resamp range, more extreme synth params")
+    parser.add_argument("--comment", type=str, default="lower dim, more views, wider range, higher synth params")
     
     args = parser.parse_args()
 
