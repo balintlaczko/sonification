b7719a56910819ee41d784ef2280e3774a547a29
Balint Laczko <balintlaczko01@gmail.com>
2025-09-26 21:55:22 +0200

    new params

--- experiments/lsm_paper/train_img_vae.py
+++ experiments/lsm_paper/train_img_vae.py
@@ -35,16 +35,16 @@ def main():
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--warmup_epochs", type=int, default=10)
     # reconstruction loss params
-    parser.add_argument('--recon_loss_type', type=str, default='mse', help='reconstruction loss type: l1 or mse')
-    parser.add_argument("--recon_loss_weight_start", type=float, default=100)
-    parser.add_argument("--recon_loss_weight_end", type=float, default=100)
+    parser.add_argument('--recon_loss_type', type=str, default='l1', help='reconstruction loss type: l1 or mse')
+    parser.add_argument("--recon_loss_weight_start", type=float, default=200)
+    parser.add_argument("--recon_loss_weight_end", type=float, default=200)
     parser.add_argument("--recon_loss_weight_ramp_start_epoch", type=int, default=0)
     parser.add_argument("--recon_loss_weight_ramp_end_epoch", type=int, default=1)
-    parser.add_argument('--target_recon_loss', type=float, default=0.001, help='target recon loss to keep in case of dynamic kld')
+    parser.add_argument('--target_recon_loss', type=float, default=0.01, help='target recon loss to keep in case of dynamic kld')
     # kld loss params
     parser.add_argument('--dynamic_kld', type=int, default=1, help='non-zero will use dynamic kld')
     parser.add_argument('--kld_weight_max', type=float, default=100, help='kld weight at the end of the warmup')
-    parser.add_argument('--kld_weight_min', type=float, default=0.1, help='kld weight at the start of the warmup')
+    parser.add_argument('--kld_weight_min', type=float, default=0.01, help='kld weight at the start of the warmup')
     parser.add_argument('--kld_start_epoch', type=int, default=0, help='the epoch at which to start the kld warmup from kld_weight_min to kld_weight_max')
     parser.add_argument('--kld_warmup_epochs', type=int, default=1, help='the number of epochs to warmup the kld weight')
     # tc loss params
@@ -53,7 +53,7 @@ def main():
     parser.add_argument('--tc_start_epoch', type=int, default=0, help='the epoch at which to start the tc warmup from tc_weight_min to tc_weight_max')
     parser.add_argument('--tc_warmup_epochs', type=int, default=1, help='the number of epochs to warmup the tc weight')
     # optimizer params
-    parser.add_argument("--lr_vae", type=float, default=0.001)
+    parser.add_argument("--lr_vae", type=float, default=0.0002)
     parser.add_argument("--lr_decay_vae", type=float, default=0.85)
     parser.add_argument("--lr_d", type=float, default=0.0001)
     parser.add_argument("--lr_decay_d", type=float, default=0.85)
@@ -61,9 +61,9 @@ def main():
     # parser.add_argument("--steps_per_epoch", type=int, default=400)
     # checkpointing & logging
     parser.add_argument("--ckpt_path", type=str, default="./ckpt/squares_vae")
-    parser.add_argument("--ckpt_name", type=str, default="imv_new_v2")
+    parser.add_argument("--ckpt_name", type=str, default="imv_new_v5")
     parser.add_argument("--logdir", type=str, default="./logs/squares_vae")
-    parser.add_argument("--comment", type=str, default="lower kld, lr_vae=0.001")
+    parser.add_argument("--comment", type=str, default="base kld to 0.01, recon weight 200, lr_vae 0.0002")
 
     args = parser.parse_args()
 
